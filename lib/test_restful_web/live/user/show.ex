10f5105861aebf3a8c9bdb618a10df964da104d5
MarilynWaldman <mdwaldman22@hotmail.com>
2020-03-01 12:24:53 -0700

    fixed show

--- lib/test_restful_web/live/user/show.ex
+++ lib/test_restful_web/live/user/show.ex
@@ -1,4 +1,5 @@
-defmodule TestRestfulWeb.UserLive.Show do
+defmodule TestRestfulWeb.UserLive.Show
+  do
   use Phoenix.LiveView
   use Phoenix.HTML
 
@@ -11,26 +12,15 @@ defmodule TestRestfulWeb.UserLive.Show do
   def render(assigns), do: UserView.render("show.html", assigns)
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, count: 0)}
   end
 
   def handle_params(%{"id" => id}, _url, socket) do
-    if connected?(socket), do: TestRestful.Accounts.subscribe(id)
-    {:noreply, socket |> assign(id: id) |> fetch()}
+    user = Accounts.get_user!(id)
+    {:noreply,
+      assign(socket, %{
+        user: user
+      })}
   end
 
-  defp fetch(%Socket{assigns: %{id: id}} = socket) do
-    assign(socket, user: Accounts.get_user!(id))
-  end
-
-  def handle_info({Accounts, [:user, :updated], _}, socket) do
-    {:noreply, fetch(socket)}
-  end
-
-  def handle_info({Accounts, [:user, :deleted], _}, socket) do
-    {:stop,
-      socket
-      |> put_flash(:error, "This user has been deleted from the system")
-      |> redirect(to: Routes.live_path(socket, TestRestfulWeb.UserLive.Index))}
-  end
 end
